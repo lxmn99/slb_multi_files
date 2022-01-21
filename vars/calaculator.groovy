1354078c3f09b3031247854081123cce2d60c4ba
lxmn99 <60390751+lxmn99@users.noreply.github.com>
2022-01-21 12:05:43 +0530

    Update calaculator.groovy

--- vars/calaculator.groovy
+++ vars/calaculator.groovy
@@ -1,9 +1,9 @@
 def add(x,y){
-echo "sum of ${x} and ${y} is ${x+y}"
+echo "this is second sum of ${x} and ${y} is ${x+y}"
 }
 def mul(x,y){
 
-echo "mul of ${x} and ${y} is ${x*y}"
+echo "this is seond mul of ${x} and ${y} is ${x*y}"
 }
 def div(x,y){
 echo "dividing funcion"
